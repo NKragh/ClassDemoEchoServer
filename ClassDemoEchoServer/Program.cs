f2a17d5a10b41b89d8e663c7ce53e26e78d50718
NKragh <39466838+NKragh@users.noreply.github.com>
2019-08-29 10:14:20 +0200

    Add project files.

---
+++ ClassDemoEchoServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassDemoEchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
